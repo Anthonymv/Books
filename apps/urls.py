5af58697b84bec85d4d3888b58c5233860a19089
Anthonymv <Anthony.delgado.ch@gmail.com>
2018-06-16 16:31:05 -0400

    Proyect Books

---
+++ apps/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('list_student/', views.list_student, name='list_student'),
+    path('create_student/', views.StudentCreate.as_view(), name='create_student'),
+    re_path(r'^edit_student/(?P<pk>\d+)/$', views.StudentEdit.as_view(), name='edit_student'),
+    re_path(r'^delete_student/(?P<pk>\d+)/$', views.StudentDelete.as_view(), name='delete_student'),
+]
---
+++ apps/views.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import CreateView, UpdateView, DeleteView
+
+from apps.models import Student
+from apps.forms import StudentForm
+
+
+# Create your views here.
+
+@login_required
+def index(request):
+    return render(request, template_name='index.html')
+
+
+def list_student(request):
+    student = Student.objects.all()
+    return render(request, template_name='list_student.html', context={'student': student})
+
+
+class StudentCreate(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'add_student.html'
+
+    def get_success_url(self):
+        return reverse('list_student')
+
+
+class StudentEdit(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'edit_student.html'
+
+    def get_success_url(self):
+        return reverse('list_student')
+
+
+class StudentDelete(DeleteView):
+    model = Student
+    form_class = StudentForm
+    template_name = 'delete_student.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super(StudentDelete, self).get_context_data(**kwargs)
+        pk = self.kwargs.get('pk')
+        student = Student.objects.get(id=int(pk))
+        context_data.update({'student': student})
+        return context_data
+
+    def get_success_url(self):
+        return reverse('list_student')
